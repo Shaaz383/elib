0c9f0b22705b89ba01ae473c3d2e1135fcb443a2
Shaaz <asadakhtar800@gmail.com>
2024-05-14 17:57:09 +0530

    password hashed

--- src/user/userController.ts
+++ src/user/userController.ts
@@ -1,6 +1,7 @@
 import { Request,Response,NextFunction } from "express";
 import createHttpError from "http-errors";
 import userModel from "./userModel";
+import bcrypt from "bcrypt"
 
 const createUser =async (req :Request,res: Response,next : NextFunction) =>{
 
@@ -21,7 +22,10 @@ const createUser =async (req :Request,res: Response,next : NextFunction) =>{
     return next(error)
   }
 
+  //Password Hash
+  const hashedPassword = await bcrypt.hash(password,10);
 
+  
   //process
   //response
 
