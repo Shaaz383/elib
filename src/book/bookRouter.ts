7fbaeb22fa688293635fcfd63d2c06f6ecfe0b14
Shaaz <asadakhtar800@gmail.com>
2024-05-15 20:08:13 +0530

    update book

--- src/book/bookRouter.ts
+++ src/book/bookRouter.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import multer from 'multer'
-import {createBook} from './bookController';
+import {createBook , updateBook} from './bookController';
 import path from 'node:path'
 import authenticate from '../middlewares/authenticate';
 
@@ -22,6 +22,16 @@ bookRouter.post('/',authenticate,upload.fields([
   {name : 'coverImage', maxCount : 1},
   {name : 'file', maxCount : 1}
 ]) , createBook);
+
+bookRouter.patch(
+  "/:bookId",
+  authenticate,
+  upload.fields([
+    { name: "coverImage", maxCount: 1 },
+    { name: "file", maxCount: 1 },
+  ]),
+  updateBook
+);
 
 
 
